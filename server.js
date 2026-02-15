3136a8d71feb74d47b1230f21eb99bd0e61e8764
pdev <pchiavarino89@gmail.com>
2026-02-14 22:09:31 -0300

    feat: add mock-auth with fake token

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const jsonServer = require('json-server')
+import jsonServer from 'json-server'
+
 const server = jsonServer.create()
 const router = jsonServer.router('data/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+
+server.post('/login', (req, res) => {
+  const { username, password } = req.body
+
+  if (username === 'admin' && password === 'admin') {
+    return res.json({
+      token: 'mock-token-123',
+      user: { id: 1, username: 'admin' }
+    })
+  }
+
+  res.status(401).json({ message: 'Invalid credentials' })
+})
 
 const PORT = process.env.PORT || 3001
 
